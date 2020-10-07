cf9c7408bca4231efbf9b2eec9264bce53b96e6e
FREDRIK OLAUSSON <fredrik.olausson12@gmail.com>
2020-10-07 11:38:34 +0200

    Testcommit

---
+++ fizzbuzz.py
@@ -0,0 +1,17 @@
+def main():
+    for i in range(1, 101):
+
+        if i == 42:
+            print("Answer to the Ultimate Question of Life, the Universe, and Everything")
+        elif i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+
+if __name__ == '__main__':
+    main()
